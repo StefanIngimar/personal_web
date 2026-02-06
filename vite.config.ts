e9dd6b7a0e5bce31d03c80d144c443d73af6614e
stefanla <stefanla@student.chalmers.se>
2026-02-06 14:19:35 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  base: "/personal_web/"
+  base: "/personal_web/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
 });
